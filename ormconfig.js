0b88fd3eb3338d0a9c2d34a4c4cc3dd69b89446e
luizpaulobarroca <luizpaulobarroca@gmail.com>
2020-12-20 01:35:28 -0300

    heroku

--- ormconfig.js
+++ ormconfig.js
@@ -12,7 +12,7 @@ module.exports = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
-  entities: [`${__dirname}/**/*.entity.{ts,js}`],
+  entities: [`${__dirname}/dist/**/*.entity.{ts,js}`],
   synchronize: false,
   dropSchema: false,
   migrationsRun: false,
